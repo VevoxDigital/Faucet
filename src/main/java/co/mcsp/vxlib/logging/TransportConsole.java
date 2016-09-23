2d0c5ae01e3d2eb0498cf4b68ea914f80a5eaf86
Matthew Struble <matt@vevox.io>
2016-09-23 11:03:25 -0700

    Created bukkit console transport

---
+++ src/main/java/co/mcsp/vxlib/logging/TransportConsole.java
@@ -0,0 +1,58 @@
+package co.mcsp.vxlib.logging;
+
+import org.bukkit.ChatColor;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Built-in transport for handing output to bukkit's console.
+ *
+ * @author Matthew Struble
+ * @since 1.10.2-r0.1
+ */
+@SuppressWarnings("unused WeakerAccess")
+public class TransportConsole implements Transport {
+
+  public static final int BACKSPACE_CHARS = 17;
+  public static final String BACKSPACES;
+
+  static {
+    String bs = "";
+    for (int i = 0; i < BACKSPACE_CHARS; i++)
+      bs += "\b";
+    BACKSPACES = bs;
+  }
+
+  private final LoggingLevel level;
+  private final String name;
+
+  public TransportConsole(LoggingLevel level) {
+    this("console", level);
+  }
+
+  public TransportConsole(String name, LoggingLevel level) {
+    this.name = name;
+    this.level = level;
+  }
+
+  @Override
+  public LoggingLevel level() {
+    return level;
+  }
+
+  @Override
+  public String name() {
+    return name;
+  }
+
+  @Override
+  public void receive(TransportedMessage msg) {
+    SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+    System.out.println(BACKSPACE_CHARS +
+        format.format(new Date()) + '-' +
+        msg.level.color() + msg.level.toString().toLowerCase() +
+        ChatColor.RESET + ": " + msg.message);
+  }
+
+}
