f551daa7c29d292d3fc399ed13ab0af2138a060f
Matthew Struble <matt@vevox.io>
2016-09-23 11:07:51 -0700

    Created CommandException class

---
+++ src/main/java/co/mcsp/vxlib/cmd/CommandException.java
@@ -0,0 +1,64 @@
+package co.mcsp.vxlib.cmd;
+
+/**
+ * Generic exception used in commands.
+ *
+ * @author Matthew Struble
+ * @since 1.10.2-r0.1
+ */
+@SuppressWarnings("unused WeakerAccess")
+public class CommandException extends Exception {
+
+  public CommandException(String msg) {
+    super(msg);
+  }
+
+  /**
+   * Exception used when an argument is unknown.
+   *
+   * @author Matthew Struble
+   * @since 1.10.2-r0.1
+   */
+  public static class UnknownArgumentException extends CommandException {
+    UnknownArgumentException(int index, String arg) {
+      super(String.format("Argument %d (\"%s\") is unknown", index + 1, arg));
+    }
+  }
+
+  /**
+   * Exception used when an argument is missing.
+   *
+   * @author Matthew Struble
+   * @since 1.10.2-r0.1
+   */
+  public static class MissingArgumentException extends CommandException {
+    MissingArgumentException(int index) {
+      super(String.format("Argument %d is missing", index + 1));
+    }
+  }
+
+  /**
+   * Exception used when there are insufficient permissions to execute a command.
+   *
+   * @author Matthew Struble
+   * @since 1.10.2-r0.1
+   */
+  public static class InsufficientPermissionsException extends CommandException {
+    InsufficientPermissionsException(String permission) {
+      super("Missing permission " + permission);
+    }
+  }
+
+  /**
+   * Exception used when a player-only command is executed by the console.
+   *
+   * @author Matthew Struble
+   * @since 1.10.2-r0.1
+   */
+  public static class ConsoleExecutionException extends CommandException {
+    ConsoleExecutionException() {
+      super("Command must be executed by a player");
+    }
+  }
+
+}
